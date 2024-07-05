31ec6739a1d5e5b9fe1a844c95c163642988aea9
SujataSethy18022005 <sujatasethy18022005@gmail.com>
2024-07-05 09:25:17 +0000

    function

---
+++ 02-advance-js/function.js
@@ -0,0 +1,49 @@
+//function myFunction(){
+  //  console.log("welcome to web bocket");
+    //console.log("hello everyone");
+//}
+
+//myFunction();
+//myFunction();
+//myFunction();
+
+//function myFunc(msg){ //parameter(input)
+//     console.log(msg)
+//}
+//myFunc("today is a great day and GIFT is best for technical education") //argument
+
+
+//function -> 2 number sum
+//function sum(a,b){
+  //  c = a + b;
+   // console.log("sum of a + b is ", c);
+//}
+//sum(2,4);
+
+
+// sum function
+//function sum(a, b){
+//    return a + b;
+//}
+
+// arrow function in SUM
+//const arrowfunc = (a, b) => {
+//    console.log(a + b);
+//}
+
+// multiplication function
+//function mul(a, b){
+//    return a * b;
+//}
+
+// arrow function in MUL
+const arrowfunc1 = (a, b) => {
+    c = a * b;
+     console.log(c);
+ }
+ arrowfunc1(3,4);
+ 
+ //Q1. creat a function using the "function" keyword that takes a strict as an argument and return the numner of values 
+ //in the string. (a,e,i,o,u)
+ 
+ //Q2. Creat an arrow function to perform same task
