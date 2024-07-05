28d627ee8016457032d82882ef7ee7faef514b81
SujataSethy18022005 <sujatasethy18022005@gmail.com>
2024-07-05 10:15:21 +0000

    function and method end

--- 02-advance-js/function.js
+++ 02-advance-js/function.js
@@ -37,13 +37,53 @@
 //}
 
 // arrow function in MUL
-const arrowfunc1 = (a, b) => {
-    c = a * b;
-     console.log(c);
- }
- arrowfunc1(3,4);
+//const arrowfunc1 = (a, b) => {
+  //  c = a * b;
+    // console.log(c);
+ //}
+// arrowfunc1(3,4);
  
  //Q1. creat a function using the "function" keyword that takes a strict as an argument and return the numner of values 
  //in the string. (a,e,i,o,u)
  
  //Q2. Creat an arrow function to perform same task
+
+
+ //forEach Loop in array
+ //let arr = [1,2,3,4,5,6,7];
+ //arr.forEach(function printVal(val) {
+  //  console.log(val)
+ //})
+ //arr.forEach((val) => {
+ //   console.log(val)
+ //})
+
+ //let arr = ["pune", "mumbai", "delhi"];
+
+ //arr.forEach((val) => {
+    //console.log(val.toUpperCase())
+ //})
+
+
+//let nums = [2,3,4,5,6];
+//nums.forEach((num) => {
+  //  console.log(num * num);
+//})
+
+
+//let nums = [67,52,39];
+//let newArr = nums.map((val) => {
+//    return val * val;
+//})
+
+//console.log(newArr);
+//console.log(nums);
+
+
+//array filter method
+// all even number from 1 to 9
+let arr = [1,2,3,4,5,6,7,8,9];
+let newArr = arr.filter((val) => {
+    return val % 2 == 0;//logics
+})
+console.log(newArr);
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # js
 A code repo for js
+for each loop in arrays:-
+
+-arr.forEach(callBackFunction)
+-callBackFunction :- here, it is a function to execute for each element in array.
+-A
+
+Map method:-
+
+- creates a new array with the results of some operation. the values its callback returns are used to form new array.
+-arr.map(callbackFunction(value,index,array))
+ex.
+let newArr = arr.map((val) => {
+    return val * 2
+})
+
+filter Method of Array:-
+-creates a new arrray of element that gives true for a condition/filter.
+ex. all even elements.
+
+let newArr = arr.filter((val) => {
+    return val % 2 == 0;
+})
